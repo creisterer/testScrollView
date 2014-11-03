bdb9b312b5e722033ffca7ffd6799498c43af7f0
Colin Reisterer <creisterer@aereo.com>
2014-11-03 10:02:54 -0500

    Use HomeViewController class with nib.

--- testScrollView/AppDelegate.m
+++ testScrollView/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "HomeViewController.h"
 
 @interface AppDelegate ()
 
@@ -18,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.rootViewController = [[UIViewController alloc] initWithNibName:@"HomeViewController" bundle:nil];
+    self.window.rootViewController = [[HomeViewController alloc] initWithNibName:@"HomeViewController" bundle:nil];
     [self.window makeKeyAndVisible];
     return YES;
 }
